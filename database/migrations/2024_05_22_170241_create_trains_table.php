e56ce0f59f2d39dbe70d430a834d9c2de5145afc
MatteD04 <matteo.dantonio04@gmail.com>
2024-05-22 20:47:06 +0200

    feat: add fakers data

--- database/migrations/2024_05_22_170241_create_trains_table.php
+++ database/migrations/2024_05_22_170241_create_trains_table.php
@@ -15,6 +15,15 @@ return new class extends Migration
     {
         Schema::create('trains', function (Blueprint $table) {
             $table->id();
+            $table->string('Azienda', 120);
+            $table->string('Codice Treno', 10);
+            $table->string('Stazione di partenza', 50);
+            $table->string('Stazione di arrivo', 50);
+            $table->string('orario di partenza', 10);
+            $table->string('orario di arrivo', 10);
+            $table->smallInteger('numero di carrozze')->unsigned();
+            $table->tinyInteger('in orario')->unsigned()->default(1);
+            $table->tinyInteger('cancellato')->unsigned()->default(1);
             $table->timestamps();
         });
     }
---
+++ database/seeders/TrainsTableSeeder.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use App\Models\Train;
+
+class TrainsTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run(Faker $faker)
+    {
+        for($i = 0; $i < 15; $i++) {
+            $newHouse = new House();
+            $newHouse->Azienda = 'TrenItalia';
+            $newHouse->Codice_Treno = $faker->ean13();
+            $newHouse->Stazione_di_partenza = $faker->city();
+            $newHouse->Stazione_di_arrivo = $faker->city();
+            $newHouse->orario_di_partenza = $faker->dateTime();
+            $newHouse->orario_di_arrivo = $faker->dateTime();
+            $newHouse->numero_di_carrozze = $faker->numberBetween(1, 50);
+            $newHouse->in_orario = $faker->randomElement([1, 0]);
+            $newHouse->cancellato = $faker->randomElement([1, 0]);
+
+            $newHouse->save();
+        }
+    }
+}
