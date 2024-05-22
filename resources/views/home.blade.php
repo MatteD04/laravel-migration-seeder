f5d51a5269fee85735270a31de7847bf94f6f30f
MatteD04 <matteo.dantonio04@gmail.com>
2024-05-22 21:10:41 +0200

    feat: add data on page

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,5 +1,19 @@
 @extends('layouts.app')
 
 @section('content')
-<h1>ciao ciao</h1>
+<h1>Treni in partenza:</h1>
+<div class="container">
+    @foreach ($trains as $train)
+        <div>
+            <div>
+                <h4>Stazione di partenza: {{ $train->Stazione di partenza }}</h4>
+                <h4>Stazione di arrivo: {{ $train->Stazione di arrivo }}</h4>
+            </div>
+            <div>
+                <h4>orario di partenza: {{ $train->orario di partenza }}</h4>
+                <h4>orario di arrivo: {{ $train->orario di arrivo }}</h4>
+            </div>
+        </div>
+    @endforeach
+</div>
 @endsection
